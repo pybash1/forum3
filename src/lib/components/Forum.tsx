47d8acc4718db748209f2854b45ddda8ecdcafa8
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2022-12-05 19:56:55 +0530

    fix: more minor bug fixes and improvements

--- src/lib/components/Forum.tsx
+++ src/lib/components/Forum.tsx
@@ -346,12 +346,22 @@ export default function Forum({
                 className="text-xs pt-2 flex flex-row justify-between"
                 style={{ color: theme.textColor || theme.accent }}
               >
-                Replying to "{replyMsg?.substring(0, 20)}{replyMsg?.substring(0, 20) !== replyMsg ? "..." : null}"
+                Replying to "{replyMsg?.substring(0, 20)}
+                {replyMsg?.substring(0, 20) !== replyMsg ? "..." : null}"
                 <button
                   style={{ color: theme.accent }}
                   onClick={() => setReply(null)}
                 >
-                  X
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-x"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                  </svg>
                 </button>
               </div>
             ) : null}
