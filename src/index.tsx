6ed71fe0d9282f6c1b76df7c178167170f886d89
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2022-12-09 17:11:13 +0530

    fix: forgot to add file in previous commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import ErrorPage from "./components/ErrorPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
