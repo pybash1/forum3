a41a712b842912346d608f42162d7c48738be3ad
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2022-12-13 20:06:31 +0530

    fix(changelog): removed placeholders from changelog and added CHANGELOG.md file

---
+++ CHANGELOG.md
@@ -0,0 +1,36 @@
+## Unreleased
+
+### Feat
+
+- **introduction**: completed introduction subpages with feedback page(without functionality)
+- added introduction page and created new sidebar for docs pages with improved dynamic navbar component
+- added routing and 404 page
+- **animation**: improved animation and added staggering
+- **meta**: improved seo and tags
+- complete landing page
+- improved landing page
+- basic landing page design
+- **notifications**: added notifications instead of console.logs
+- added sender address
+- **replies**: improved replies ui with link detection and styling
+- added link detection and basic styling options thru html
+- added basic reply functionality and ui
+- **reply**: added reply ui
+- **scroll**: added auto scroll to bottom of messages
+- **message**: now messages can be seen and only sent in custom context
+- added functionality to create posts/messages
+- **forum**: forum component with connection functionality
+- **init**: generated project structure and boilerplate
+
+### Fix
+
+- minor bug fixes, improvements and layout changes
+- forgot to add file in previous commit
+- updated assets, manifest, and sources
+- fixed major ui bug where background was behind other text
+- fixed minor bugs and issues and made minor improvements
+- more minor bug fixes and improvements
+- fix some minor bugs and formatting
+- **colors**: now notifications match theme
+- **scroll**: fixed autoscroll, now it scrolls only once, and after each new message sent
+- fixed bug where context provided as prop was not being used
--- src/routes/introduction/Changelog.tsx
+++ src/routes/introduction/Changelog.tsx
@@ -13,9 +13,7 @@ export default function Changelog() {
         ]}
       />
       <div className="pl-[280px] w-2/3 pt-20 font-inter">
-        <div className="font-bold text-black text-4xl pt-16">
-          What's New
-        </div>
+        <div className="font-bold text-black text-4xl pt-16">What's New</div>
         <div className="text-[#464168] text-xl pt-5 pb-10 font-normal">
           We constantly publish improvements and keep track of changes here.
         </div>
@@ -39,10 +37,66 @@ export default function Changelog() {
         <div className="font-bold text-xl">Unreleased</div>
         <div className="text-[#464168] pt-5 pb-10 font-normal">
           <ul className="list-disc pl-4">
-            <li>TODO: Add Changelog when done</li>
-            <li>TODO: Add Changelog when done</li>
-            <li>TODO: Add Changelog when done</li>
-            <li>TODO: Add Changelog when done</li>
+            <li>
+              <b>introduction</b>: completed introduction subpages with feedback
+              page(without functionality)
+            </li>
+            <li>
+              added introduction page and created new sidebar for docs pages
+              with improved dynamic navbar component
+            </li>
+            <li>added routing and 404 page</li>
+            <li>
+              <b>animation</b>: improved animation and added staggering
+            </li>
+            <li>
+              <b>meta</b>: improved seo and tags
+            </li>
+            <li>complete landing page</li>
+            <li>improved landing page</li>
+            <li>basic landing page design</li>
+            <li>
+              <b>notifications</b>: added notifications instead of console.logs
+            </li>
+            <li>added sender address</li>
+            <li>
+              <b>replies</b>: improved replies ui with link detection and
+              styling
+            </li>
+            <li>added link detection and basic styling options thru html</li>
+            <li>added basic reply functionality and ui</li>
+            <li>
+              <b>reply</b>: added reply ui
+            </li>
+            <li>
+              <b>scroll</b>: added auto scroll to bottom of messages
+            </li>
+            <li>
+              <b>message</b>: now messages can be seen and only sent in custom
+              context
+            </li>
+            <li>added functionality to create posts/messages</li>
+            <li>
+              <b>forum</b>: forum component with connection functionality
+            </li>
+            <li>
+              <b>init</b>: generated project structure and boilerplate
+            </li>
+            <li>minor bug fixes, improvements and layout changes</li>
+            <li>forgot to add file in previous commit</li>
+            <li>updated assets, manifest, and sources</li>
+            <li>fixed major ui bug where background was behind other text</li>
+            <li>fixed minor bugs and issues and made minor improvements</li>
+            <li>more minor bug fixes and improvements</li>
+            <li>fix some minor bugs and formatting</li>
+            <li>
+              <b>colors</b>: now notifications match theme
+            </li>
+            <li>
+              <b>scroll</b>: fixed autoscroll, now it scrolls only once, and
+              after each new message sent
+            </li>
+            <li>fixed bug where context provided as prop was not being used</li>
           </ul>
         </div>
       </div>
