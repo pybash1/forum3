754a464889839f590d523e742e9d6e15178129d1
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2022-12-22 11:44:53 +0530

    feat(images): added prop to disable image sharing

--- src/lib/components/Forum.tsx
+++ src/lib/components/Forum.tsx
@@ -45,6 +45,7 @@ export default function Forum({
   closedText,
   admins = [],
   indicator = true,
+  allowImages = true,
   context = process.env.REACT_APP_FORUM_CONTEXT || "",
 }: Props) {
   const [user, setUser] = useState<string>();
@@ -245,7 +246,7 @@ export default function Forum({
             "th",
             "thead",
             "tr",
-            "img",
+            allowImages ? "img" : "tr",
           ],
           allowedAttributes: {
             a: ["href", "name", "target"],
@@ -668,26 +669,28 @@ export default function Forum({
                 accept="image/*"
                 onChange={() => setVisible(true)}
               />
-              <button
-                onClick={trigger}
-                className="rounded-full p-2"
-                style={{
-                  background: theme.buttonColor || theme.accent,
-                  color: theme.background,
-                }}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-upload"
-                  viewBox="0 0 16 16"
+              {allowImages ? (
+                <button
+                  onClick={trigger}
+                  className="rounded-full p-2"
+                  style={{
+                    background: theme.buttonColor || theme.accent,
+                    color: theme.background,
+                  }}
                 >
-                  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
-                  <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z" />
-                </svg>
-              </button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-upload"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                    <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z" />
+                  </svg>
+                </button>
+              ) : null}
               <button
                 onClick={send}
                 className="rounded-full p-2"
@@ -739,6 +742,7 @@ interface Props {
   iconOnly?: boolean;
   admins?: string[];
   indicator?: boolean;
+  allowImages?: boolean;
 }
 
 interface Colors {
